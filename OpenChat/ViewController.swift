e50977cac8898c44d3003d759f0a4dbbcd976eea
JunseoThomasPark <parkaph@bc.edu>
2022-04-26 17:36:29 -0400

    Initial Commit

---
+++ OpenChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenChat
+//
+//  Created by Thomas Park on 4/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
